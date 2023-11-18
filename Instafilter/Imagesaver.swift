2cf9da776df52d5fca82e0bf5bdf8ee2d5f23394
Rehman Nahimi <rehman2000@live.com>
2023-11-18 12:04:55 +0000

    Save image to album API

--- Instafilter/Imagesaver.swift
+++ Instafilter/Imagesaver.swift
@@ -5,6 +5,16 @@
 //  Created by Ray Nahimi on 12/11/2023.
 //
 
-import PhotosUI
-import SwiftUI
+import UIKit
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage){
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo:
+        UnsafeRawPointer){
+        print("Save Finished")
+    }
+}
 
